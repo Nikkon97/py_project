805202e01d73036aa849b1f910ecb330f7071234
Артем Никонов <player1997@mail.ru>
2023-02-21 17:51:44 +0300

    import add

---
+++ main.py
@@ -0,0 +1,6 @@
+from funcs import add
+
+if __name__ == '__main__':
+    first_number = int(input('Введите первое число '))
+    second_number = int(input('Введите второе число '))
+    print(add(first_number, second_number))
