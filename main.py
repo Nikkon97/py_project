93e9151f46aa319fdab113d148320d2826c864d4
Артем Никонов <player1997@mail.ru>
2023-02-21 17:53:24 +0300

    import subtract

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from funcs import add
+from funcs import add, subtract
 
 if __name__ == '__main__':
     first_number = int(input('Введите первое число '))
     second_number = int(input('Введите второе число '))
     print(add(first_number, second_number))
+    print(subtract(first_number, second_number))
